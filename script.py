abeb495eb18efcf2f222aa393702d5136fa93a97
lamine87 <laminenatidiarra@yahoo.fr>
2024-01-17 11:43:17 +0100

    Add V2.3

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ import numpy as numpy
 def main(): 
     a = int(os.environ["a"])
     b = int(os.environ["b"])
+    print(a)
+    print(b)
     print("Affiche A * B : " (a*b))
     # valeurAB = float(os.environ["A"]) * float(os.environ["B"])
     # print("Affiche A * B : " + str(valeurAB))
