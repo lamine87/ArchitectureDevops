7d7526b0da8ef6a063bbab208325992dcf1c98f9
lamine87 <laminenatidiarra@yahoo.fr>
2024-01-17 11:03:04 +0100

    Add V1.6

--- script.py
+++ script.py
@@ -2,11 +2,11 @@ import os
 import numpy as numpy
 
 def main(): 
-    a = float(os.environ["A"])
-    b= float(os.environ["B"])
+    a = float(os.environ["MYVAR1"])
+    b= float(os.environ["MYVAR"])
     print("Affiche A * B : " (a*b))
-    valeurAB = float(os.environ["A"]) * float(os.environ["B"])
-    print("Affiche A * B : " + str(valeurAB))
+    # valeurAB = float(os.environ["A"]) * float(os.environ["B"])
+    # print("Affiche A * B : " + str(valeurAB))
     
 
 if __name__ == "__main__":
