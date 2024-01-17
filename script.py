997c6920cd835974395f03c0d2de5b1cf6a4b457
lamine87 <laminenatidiarra@yahoo.fr>
2024-01-17 11:49:47 +0100

    Add V2.4

--- script.py
+++ script.py
@@ -2,13 +2,20 @@ import os
 import numpy as numpy
 
 def main(): 
+        
+    # valeurAB = float(os.environ["a"]) * float(os.environ["b"])
+    # print("Affiche A * B : " + str(valeurAB))
+    
     a = int(os.environ["a"])
     b = int(os.environ["b"])
+    ab = a*b
     print(a)
     print(b)
-    print("Affiche A * B : " (a*b))
-    # valeurAB = float(os.environ["A"]) * float(os.environ["B"])
-    # print("Affiche A * B : " + str(valeurAB))
+    print(ab)
+    print("Affiche A * B : " +ab)
+    
+    print(f'log(ab) = {numpy.log(ab)}')
+    print(f'log(a) + log(b) = {numpy.log(a) + numpy.log(b)}')
     
 
 if __name__ == "__main__":
