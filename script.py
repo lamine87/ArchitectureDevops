5e89b239e5a20ce52307c2875ee77583571eccee
lamine87 <laminenatidiarra@yahoo.fr>
2024-01-17 08:56:04 +0100

    Add VAR9

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 
 def main():
-    valeurAB = float(os.environ["A"])*float(os.environ["B"])
-    print("Affiche A * B : " + valeurAB)
-     
+    valeurAB = float(os.environ["A"]) * float(os.environ["B"])
+    print("Affiche A * B : " + str(valeurAB))
 
 if __name__ == "__main__":
     main()
+
